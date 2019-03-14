e262b8cd5e1fe2d6114c510390e72dd81ff0e489
Danny G <contact@dannygallagher.org>
2019-03-14 11:16:58 +0000

    HLoadAsync

--- hLevelsRuntime/HLoad.cs
+++ hLevelsRuntime/HLoad.cs
@@ -7,12 +7,22 @@ namespace hLevelsRuntime
 {
     public static class HLoad
     {
-        public static void LoadAdditiveScenesFromList(string[] levelList)
+        public static IPromise LoadAdditiveScenesFromList(string[] levelList)
         {
+            var promise = new Promise();
             var asyncLoadList = new IPromise[levelList.Length];
 
             for (int index = 0; index < levelList.Length; index++)
                 asyncLoadList[index] = PromiseLoadScene(levelList[index], LoadSceneMode.Additive);
+
+            Promise.All(asyncLoadList)
+                .Then(() =>
+                {
+                    Debug.Log("Scenes are all loaded.");
+                })
+                .Finally(() => promise.Resolve());
+
+            return promise;
         }
 
         private static IPromise PromiseLoadScene(string sceneName, LoadSceneMode loadMode)
